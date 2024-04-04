9c7086a7b4d270c561ce773d3a46b57f36704b07
railgnam <57008999+railgnam@users.noreply.github.com>
2024-04-04 11:21:45 +0200

    update program_1
    tested first version.
    run it in terminal.

--- program_1.py
+++ program_1.py
@@ -2,6 +2,8 @@
 first draft
 generates output in terminal
 """
+import time
+
 # settings
 ROWS = 100
 RULE = 109
@@ -11,31 +13,46 @@ WIDTH = 100
 # CREATE A CODE DICTONARY
 # make a rule
 # 8 output digit
-rule = '01010011'
+code = '{0:08b}'.format(RULE)
+print('\nRULE: ', RULE, '>>', code)
 
 # prepare the 8 input_cases for the rule
-input_cases = ['000', '001',...]
+input_cases_str = []
+for i in range(8):
+    rule_variation_string = '{0:03b}'.format(i)
+    input_cases_str.append(rule_variation_string)
 
 # code dictionary of the rule
-code_dict = {input_cases[i] : rule[i]}
+rule_dict = {}
+for i, input_case in enumerate(input_cases_str):
+    rule_dict[input_case] = code[i]
 
 
 # FIRST LINE
 # set up first_line using binary formating
-first_line = '00000000000001'
-print(first_line)
+first_line = '{0:0100b}'.format(int(1)) 
+draw = first_line.replace('1', '_')
+print(draw)
 
 
 # PLAY THE RULE
-for _ in range(ROWS):
-    pass
-    new_line = ''
-    for i in range(len(first_line)):
-        
-        new_line += 'new_digit'
+guideline = first_line
+newline = ''
+
+for i in range(ROWS):
+    time.sleep(0.01) # animation effect
+    L = len(guideline)
+    for i in range(L):
+        # match rule dictonary key with the digits above
+        case = guideline[i - 1] + guideline[i] + guideline[(i + 1) % L]
+        newline += rule_dict[case]
     
+    guideline = newline
+    newline = ''
+
     # PRINT THE STRING
-    print(new_line)
+    draw = guideline.replace('1', '_')
+    print(draw)
     
 
 
